81760d683d522808b29915f43aec5462dfcba94c
ripunjoym <ripun170107053@iitg.ac.in>
2021-06-22 12:43:53 +0530

    1

--- src/components/CC.js
+++ src/components/CC.js
@@ -77,7 +77,7 @@ var chartData2 = [
   },
   {
     label: "Canada",
-    value: "180"
+    value: "180.45"
   },
   {
     label: "Iran",
@@ -105,7 +105,7 @@ var chartData2 = [
 // console.log(typeof chartData2);
 // Create a JSON object to store the chart configurations
 const chartConfigs = {
-  type: "column2d", // The chart type
+  type: "line", // The chart type
   width: "700", // Width of the chart
   height: "400", // Height of the chart
   dataFormat: "json", // Data type
@@ -116,32 +116,33 @@ const chartConfigs = {
       subCaption: "",             //Set the chart subcaption
       xAxisName: "Time",           //Set the x-axis name
       yAxisName: "USD",  //Set the y-axis name
-      numberSuffix: "K",
+      numberSuffix: "$",
+      valuePosition: "Amazon",
       theme: "fusion"                 //Set the theme for your chart
     },
     // Chart Data - from step 2
     data: chartData
   }
 };
-const chartConfigs2 = {
-  type: "column2d", // The chart type
-  width: "700", // Width of the chart
-  height: "400", // Height of the chart
-  dataFormat: "json", // Data type
-  dataSource: {
-    // Chart Configuration
-    chart: {
-      caption: "Stock Prices Data",    //Set the chart caption
-      subCaption: "",             //Set the chart subcaption
-      xAxisName: "Time",           //Set the x-axis name
-      yAxisName: "USD",  //Set the y-axis name
-      numberSuffix: "K",
-      theme: "fusion"                 //Set the theme for your chart
-    },
-    // Chart Data - from step 2
-    data: chartData2
-  }
-};
+// const chartConfigs2 = {
+//   type: "column2d", // The chart type
+//   width: "700", // Width of the chart
+//   height: "400", // Height of the chart
+//   dataFormat: "json", // Data type
+//   dataSource: {
+//     // Chart Configuration
+//     chart: {
+//       caption: "Stock Prices Data",    //Set the chart caption
+//       subCaption: "",             //Set the chart subcaption
+//       xAxisName: "Time",           //Set the x-axis name
+//       yAxisName: "USD",  //Set the y-axis name
+//       numberSuffix: "K",
+//       theme: "fusion"                 //Set the theme for your chart
+//     },
+//     // Chart Data - from step 2
+//     data: chartData2
+//   }
+// };
 class CC extends React.Component 
 {
   constructor()
@@ -168,6 +169,7 @@ class CC extends React.Component
       // console.log(companiesToDisplay);
        this.showChart=true;
     }
+  
   
   render() 
   {
