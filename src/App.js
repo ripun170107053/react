5cad8da02d25c235d14d4eaece33773f8c99b304
ripunjoym <ripun170107053@iitg.ac.in>
2021-06-22 15:05:55 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import './App.css';
 
