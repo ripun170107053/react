baffbcd98525a241a75c9798bd3981049140b21e
ripunjoym <ripun170107053@iitg.ac.in>
2021-06-22 15:03:12 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './App.css';
@@ -6,19 +7,32 @@ import './App.css';
 import AdminNavbar from './components/AdminNavbar';
 import UserNavbar from './components/UserNavbar';
 
-function App() {
+class App extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      admin: false,
+    };
+  }
+  render()
+  {
+    return (
+      <Router>
+      <div className="wrapper">
+        <h1>Stock Market App</h1>
+        <li className="nav-item">
+                <Link to="/admin">Login as Admin</Link>
+                <Link to="/user"> Login as User</Link>
+        </li>
+        <Route exact path="/admin" component={AdminNavbar} />
+        <Route exact path="/user" component={UserNavbar} />
+      </div>
+      </Router>
+      
+    );
+  }
   
-  return (
-    <div className="wrapper">
-      <h1>Stock Market App</h1>
-      <li className="nav-item">
-              <Link to="/admin">Login as Admin</Link>
-              <Link to="/user"> Login as User</Link>
-      </li>
-      <Route exact path="/admin" component={AdminNavbar} />
-      <Route exact path="/user" component={UserNavbar} />
-    </div>
-    
-  );
 }
 export default App;
