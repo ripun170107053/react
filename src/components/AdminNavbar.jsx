54ee9b6a0b7260a8b0a6a381275e640898aecf58
ripunjoym <ripun170107053@iitg.ac.in>
2021-06-28 11:03:39 +0530

    7

--- src/components/AdminNavbar.jsx
+++ src/components/AdminNavbar.jsx
@@ -69,6 +69,7 @@ const AdminNavbar=()=>
                 <form className="form-inline my-2 my-lg-0">
                     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
                         <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </input>
                 </form>
             </div>
         </nav>
