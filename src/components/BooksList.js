c0f5b0de1f55762bd107d4d36ccb930bcff3458e
ripunjoym <ripun170107053@iitg.ac.in>
2021-06-29 11:10:59 +0530

    1

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -2,12 +2,14 @@ import React, { useContext } from 'react';
 import _ from 'lodash';
 import Book from './Book';
 import BooksContext from "../context/BooksContext";
+import IpoService from "../services/IpoService";
 
 const BooksList = () => {
   const { books, setBooks } = useContext(BooksContext);
 
   const handleRemoveBook = (id) => {
     setBooks(books.filter((book) => book.id !== id));
+    IpoService.deleteIpo(id).then(r => console.log("delete success"));
   };
 
   return (
